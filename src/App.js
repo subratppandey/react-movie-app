1840158d082f36e4e6463541f46ef60b856df89a
Subrat Pandey <subratpandey974@gmail.com>
2022-09-16 21:27:17 -0500

    created three react components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+    if(responseJson.Search){
+      setMovies(responseJson.Search);
+    }
+  };
+
+  useEffect (() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container_fluid movie_app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+          <MovieListHeading  heading= "Movies" />
+          <SearchBox searchValue= {searchValue} setSearchValue= {setSearchValue} />
+      </div>
+
+      <div className="row">
+      <MovieList movies = {movies} />
+      </div>
+
     </div>
   );
-}
+};
 
 export default App;
